d1651a76b58cd6cae19d6e7d8f39a9db6d479b03
paulo <lpbalves7filho@gmail.com>
2024-08-24 10:36:46 -0300

    chore: added prisma ORM and models

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+    "@rocketseat/eslint-config/node"
   ],
   root: true,
   env: {
